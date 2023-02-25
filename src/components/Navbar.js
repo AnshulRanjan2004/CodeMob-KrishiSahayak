1d2a5fab5107459aaee907eda585afef72a1de4b
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-02-25 15:30:02 +0530

    Created navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,18 @@ import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-
-
+  state = {clicked : false};
+  handleClick = () =>{
+    this.setState({clicked : !this.state.clicked})
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Krishi Sahayak</h1>
-        <ul className="nav-menu">
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars" }></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
