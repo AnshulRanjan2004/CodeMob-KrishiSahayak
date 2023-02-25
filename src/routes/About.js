1d51b9771cb370eaa660233cc502193865250566
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-02-25 16:39:01 +0530

    added backend flask server

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function About(){
+    return(
+        <>
+            <Navbar/>
+            <h1>This is About</h1>
+        </>
+    )
+}
+
+export default About;
